549b2cbfc42c73f59cc65431d106baba3c7e97ee
Jeffrey Lu <Jeffrey@Jeffreys-MacBook-Pro-4.local>
2017-01-02 12:49:51 -0500

    Added captor colors to mountain watching

--- src/mountainsWatcher.js
+++ src/mountainsWatcher.js
@@ -29,8 +29,29 @@ var MountainsWatcher = (function() {
         return result;
     }
 
-    function markCell(cell) {
+    var colors = {};
+    
+    function markCellWhite(cell) {
         cell.style.outline = '3px dashed white';
+    }
+    
+    function markCell(cell) {
+        cell.style.outline = '3px dashed ' + getCapturedColor();
+    }
+    
+    function getCapturedColor() {
+        var messages = document.getElementsByClassName('server-chat-message');
+        // iterate backwards through messages, find first message that matches "A" captured "B."
+        var re = /(.+)\scaptured\s(.+)\./;
+        for (var i = messages.length - 1; i >= 0; i--) {
+            var matches = re.exec(messages[i].innerHTML);
+            if (matches && colors.hasOwnProperty(matches[1])) {
+                return colors[matches[1]];
+            }
+        }
+
+        // something went wrong, default to white
+        return 'white';
     }
 
     var watchIntvl = 0;
@@ -42,20 +63,44 @@ var MountainsWatcher = (function() {
 
         try {
             var initialMountains = getCityCells();
+            
+            // get player colors
+            var lb = document.getElementsByClassName('leaderboard-name');
+            for (var i = 0; i < lb.length; i++) {
+                var player = lb[i].innerHTML;
+                var classes = lb[i].className.split(/[ ]+/);
+                colors[player] = classes[classes.length - 1];
+            }
+            
             console.log('initial mountains: ', initialMountains);
             console.log('watching mountains...');
         } catch (ex) {
             console.log('failed to start');
             return;
         }
+        
+        var deadGenerals = [];
 
         watchIntvl = setInterval(function() {
             try {
                 var currentMountains = getCityCells();
                 var newMountains = currentMountains.filter(idx => !initialMountains.contains(idx));
+                
+                // only mark new dead locations
+                var newDeadGenerals = newMountains.filter(idx => !deadGenerals.contains(idx))
+                for (var i = 0; i < newMountains.length; i++) {
+                    deadGenerals.push(newDeadGenerals[i]);
+                }
+                
                 var cells = getCells();
                 //        console.log('new mountains:', newMountains);
-                newMountains.map(idx => cells[idx]).forEach(markCell);
+                
+                // if more than one general is captured, it is ambiguous and we mark those cells white
+                if (newDeadGenerals.length > 1) {
+                    newDeadGenerals.map(idx => cells[idx]).forEach(markCellWhite);
+                } else {
+                    newDeadGenerals.map(idx => cells[idx]).forEach(markCell);
+                }
             } catch(ex) {
                 stop();
             }
